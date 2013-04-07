acd4931fe091147964c095301714e096bf881073
Yunze Deng <yunzed@gmail.com>
2013-04-07 13:40:46 +0800

    trival.

--- src/com/dyz/ProtoLog.java
+++ src/com/dyz/ProtoLog.java
@@ -3,11 +3,11 @@ package com.dyz;
 public class ProtoLog {
 	public static void log(String msg) {
 		long threadid = Thread.currentThread().getId();
-		System.out.println("YYANDSDK - " + threadid + " - " + msg);
+		System.out.println("LIBANDNET - " + threadid + " - " + msg);
 	}
 	
 	public static void error(String msg) {
 		long threadid = Thread.currentThread().getId();
-		System.out.println("YYANDSDK - ERROR - " + threadid + " - " + msg);
+		System.out.println("LIBANDNET - ERROR - " + threadid + " - " + msg);
 	}
 }
